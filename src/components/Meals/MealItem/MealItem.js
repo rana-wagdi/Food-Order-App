5b43c7358add0be4b6c245d992c17e60db66225f
Rana Wagdi <73128713+rana2020701@users.noreply.github.com>
2021-08-13 06:52:23 -0700

    add input

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,8 @@
+import MealItemForm from './MealItemForm';
+
 import classes from './MealItem.module.css'
+
+
 
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +14,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
         <div>
-
+            <MealItemForm />
         </div>
 
 
